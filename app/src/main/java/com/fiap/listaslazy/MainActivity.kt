50688e37ef680f4ba4d29384c33838d767e398d1
Eder Soares Sena <eder.sena@live.com>
2023-08-30 22:03:23 -0300

    Initial commit

---
+++ app/src/main/java/com/fiap/listaslazy/MainActivity.kt
@@ -0,0 +1,167 @@
+package com.fiap.listaslazy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.fiap.listaslazy.model.Game
+import com.fiap.listaslazy.repository.getGamesByStudio
+import com.fiap.listaslazy.ui.theme.ListasLazyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ListasLazyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GamesScreen()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GamesScreen() {
+    var studioState by remember {
+        mutableStateOf("")
+    }
+
+    var listGamesByStudio by remember {
+        mutableStateOf(getGamesByStudio(studio = studioState))
+    }
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(
+            text = "Meus jogos favoritos",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+        OutlinedTextField(
+            value = studioState,
+            onValueChange = {
+                studioState = it
+                listGamesByStudio = getGamesByStudio(studio = studioState)
+            },
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Nome do estúdio")
+            },
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        listGamesByStudio = getGamesByStudio(studio = studioState)
+                    }) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = ""
+                    )
+                }
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyRow() {
+            items(listGamesByStudio) {
+                StudioCard(game = it)
+            }
+        }
+        Spacer(modifier = Modifier.height(15.dp))
+        LazyColumn() {
+            items(listGamesByStudio) {
+                GameCard(game = it)
+            }
+        }
+    }
+}
+
+@Composable
+fun StudioCard(game: Game) {
+    Card(modifier = Modifier
+        .size(100.dp)
+        .padding(end = 4.dp)) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(text = game.studio)
+        }
+    }
+}
+
+@Composable
+fun GameCard(game: Game) {
+    Card(modifier = Modifier.padding(bottom = 8.dp)) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .weight(3f)
+            ) {
+                Text(
+                    text = game.title,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = game.studio,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal
+                )
+            }
+            Text(
+                text = game.releaseYear.toString(),
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Blue
+            )
+        }
+    }
+}
